deb6b2832e46a2e7b9446e50df76c53640a352f9
Antonio Pierro <antonio.pierro@gmail.com>
2015-03-05 18:02:48 +0100

    Added steteful directive and his test

---
+++ app/directives/stateful.directive.js
@@ -0,0 +1,24 @@
+/*global app*/
+app.directive('nsStateful', function () {
+    'use strict';
+    return {
+        restrict: 'A', // only matches attribute name
+        scope: false,
+        // Creating a Directive that Manipulates the DOM
+        link: function link(scope, element, attrs) {
+            if (!attrs.nsStateful) {
+                throw 'You must provide a class name in order to use the nsStateful directive.';
+            }
+
+            element.bind('click', function () {
+                scope.$apply(function () {
+                    if (!element.hasClass(attrs.nsStateful)) {
+                        element.addClass(attrs.nsStateful);
+                    } else {
+                        element.removeClass(attrs.nsStateful);
+                    }
+                });
+            });
+        }
+    };
+});
